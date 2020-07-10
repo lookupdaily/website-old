24c0d1766ea8aa88a720dcbac5a6658717c43f7a
Liz Daly <liz7daly@gmail.com>
2020-07-10 18:53:40 +0100

    Initial Commit
    Initialise Gatsby project.

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from '../components/header'
+import './index.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Get out now!</h1>
+      <p>Outside Lies Magic.</p>
+    </div>
+  )
+}
