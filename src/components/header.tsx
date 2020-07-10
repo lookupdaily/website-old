06688233238f88632427a84b8980dd127d872f69
Liz Daly <liz7daly@gmail.com>
2020-07-10 19:50:29 +0100

    Refactor header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 interface TitleProps { text: string }
 
-export function Header(props: TitleProps) {
+export const Header = (props: TitleProps) => {
   return (
     <div>
       <h1>{props.text}</h1>
