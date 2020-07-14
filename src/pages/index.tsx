ff9034a906b079163af257afa4d5fec2b700f818
Liz Daly <liz7daly@gmail.com>
2020-07-14 11:34:44 +0100

    Use layout wrapper in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import React from "react"
+import { Layout } from "../components/layout/layout"
 import { Header } from "../components/header"
 
 export default function Home() {
   return (
-    <div>
+    <Layout>
       <Header text="Get Out Now!" />
       <p>Outside Lies Magic.</p>
       <p>Publish this</p>
-    </div>
+    </Layout>
   )
 }
